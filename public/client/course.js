f06ea7c8a2f9d92686261ec543883c1b28c7b4c2
Zach Warren <zachary@wl-82-160.wireless.umt.edu>
2015-12-09 14:11:56 -0700

    Updated Users / Course page

--- public/client/course.js
+++ public/client/course.js
@@ -5,8 +5,11 @@ import { Router, Route, IndexRoute, Link } from 'react-router'
 var Course = React.createClass({
 	render: function(){
 		return(
-			<div>
-				<h2> My Course Page </h2>
+			<div className="text-center vertical-center" style={{height: '85vh', width: '100%'}}>
+				<div style={{width: '100%'}}>
+				<p><i>Your Course Information</i></p>
+				<h2> COMING SOON</h2>
+				</div>
 			</div>
 			)
 	}
--- public/client/users.js
+++ public/client/users.js
@@ -5,8 +5,11 @@ import { Router, Route, IndexRoute, Link } from 'react-router'
 var Users = React.createClass({
 	render: function(){
 		return(
-			<div>
-				<h2> USERS GO HERE!!!!</h2>
+			<div className="text-center vertical-center" style={{height: '85vh', width: '100%'}}>
+				<div style={{width: '100%'}}>
+				<p><i>Registered Users</i></p>
+				<h2> COMING SOON</h2>
+				</div>
 			</div>
 			)
 	}
